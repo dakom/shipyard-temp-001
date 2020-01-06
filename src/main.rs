7256172c64afc96f19761f7199328613821cd6f1
David Komer <david.komer@gmail.com>
2020-01-06 23:14:07 +0200

    non-vec

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use shipyard::prelude::*;
-use futures_signals::signal_vec::{SignalVec, SignalVecExt, MutableVec, VecDiff};
+use futures_signals::signal::{Signal, SignalExt, Mutable};
 use lazy_static::lazy_static;
 use futures::future::ready;
 use futures::executor::block_on;
 
-struct EntityList (pub MutableVec<EntityId>);
+struct EntityContainer (pub Mutable<Option<EntityId>>);
 struct Label (pub String);
 
 lazy_static! {
     pub static ref WORLD:World = {
         let world = World::new::<(Label, )>();
-        world.add_unique(EntityList(MutableVec::new()));
+        world.add_unique(EntityContainer(Mutable::new(None)));
         world
     };
 }
@@ -29,13 +29,8 @@ fn main() {
     add_entity();
 
     //spawn the future to run the signal listener
-    block_on(sig.for_each(|change| {
-        match change {
-            VecDiff::Push { value } => {
-                println!("[GOT LABEL] {}", value);
-            },
-            _ => {}
-        }
+    block_on(sig.for_each(|value| {
+        println!("[GOT LABEL] {}", value);
         ready(())
     }));
 }
@@ -45,34 +40,40 @@ fn add_entity() {
         entities.add_entity(&mut labels, Label("hello!".to_string()))
     });
 
-    WORLD.run::<Unique<&mut EntityList>, _, _>(|list| {
-        list.0.lock_mut().push(entity);
+    WORLD.run::<Unique<&mut EntityContainer>, _, _>(|list| {
+        *list.0.lock_mut() = Some(entity);
     });
 }
 
 // in the good approach, we get the signal first
 // then in its map function we run the world again
-fn approach_good() -> impl SignalVec<Item = String> {
-    let list_signal = WORLD.run::<Unique<&EntityList>, _, _>(|list| {
-        list.0.signal_vec()
+fn approach_good() -> impl Signal<Item = String> {
+    let container_signal = WORLD.run::<Unique<&EntityContainer>, _, _>(|container| {
+        container.0.signal()
     });
 
-    list_signal.map(|entity_id| {
+    container_signal.map(|entity_id| {
         println!("getting label for entity {:?}", entity_id);
         WORLD.run::<&Label, _, _>(|labels| {
-            (labels).get(entity_id).unwrap().0.to_string()
+            match entity_id {
+                None => "nothing".to_string(),
+                Some(entity_id) => (labels).get(entity_id).unwrap().0.to_string()
+            }
         })
     })
 }
 
 // in the bad approach we run the World
 // and then inside of that create our signal (and the map uses the views from same run)
-fn approach_bad() -> impl SignalVec<Item = String> {
-    WORLD.run::<(Unique<&EntityList>, &Label), _, _>(|(list, labels)| {
-        list.0.signal_vec()
+fn approach_bad() -> impl Signal<Item = String> {
+    WORLD.run::<(Unique<&EntityContainer>, &Label), _, _>(|(container, labels)| {
+        container.0.signal()
             .map(move |entity_id| {
                 println!("getting label for entity {:?}", entity_id);
-                (&labels).get(entity_id).unwrap().0.to_string()
+                match entity_id {
+                    None => "nothing".to_string(),
+                    Some(entity_id) => (&labels).get(entity_id).unwrap().0.to_string()
+                }
             })
     })
 }
